6953868c5baa7b9ade27dbd6facda84a34901b51
martinsw01 <martin.s.winther@gmail.com>
2022-03-01 15:16:08 +0100

    Renamed file to ode_solver and changed name of `ode_solver` to `solve_ode`

--- ode_solver.py
+++ ode_solver.py
@@ -1,30 +1,33 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+
 from variables import A0, tauB, IC
+
 
 def f(x, y):
     theta, w = y
     tau = tauB(theta, A0)
-    return np.array([w, tau/IC])
+    return np.array([w, tau / IC])
+
 
 def euler(f, x, y, h):
-    y_next = y + h*f(x, y)
+    y_next = y + h * f(x, y)
     x_next = x + h
     return x_next, y_next
 
 
 def runge_kutta_4(f, x, y, h):
     k1 = f(x, y)
-    k2 = f(x + h/2, y + h/2*k1)
-    k3 = f(x + h/2, y + h/2*k2)
-    k4 = f(x + h, y + h*k3)
+    k2 = f(x + h / 2, y + h / 2 * k1)
+    k3 = f(x + h / 2, y + h / 2 * k2)
+    k4 = f(x + h, y + h * k3)
 
-    y_next = y + h/6*(k1 + 2*k2 + 2*k3 + k4)
+    y_next = y + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
     x_next = x + h
     return x_next, y_next
 
 
-def ode_solver(f, x0, xend, y0, h, method=euler):
+def solve_ode(f, x0, xend, y0, h, method=euler):
     # Initializing:
     y_num = np.array([y0])  # Array for the solution y
     x_num = np.array([x0])  # Array for the x-values
@@ -42,18 +45,23 @@ def ode_solver(f, x0, xend, y0, h, method=euler):
 
     return x_num, y_num
 
-if __name__ == '__main__':
+
+def main():
     x0 = 0
     xend = 20
-    y0 = np.array([20/180*np.pi, 0])
+    y0 = np.array([20 / 180 * np.pi, 0])
     h = 0.001
-    x_num, y_num = ode_solver(f, x0, xend, y0, h)
+    x_num, y_num = solve_ode(f, x0, xend, y0, h)
 
     x = np.linspace(x0, xend, 101)
     plt.title("Numerical solution")
-    plt.plot(x_num, y_num[:,0], 'r')
-    plt.plot(x_num, y_num[:,1], 'b')
+    plt.plot(x_num, y_num[:, 0], 'r')
+    plt.plot(x_num, y_num[:, 1], 'b')
     plt.xlabel('x')
     plt.ylabel('y')
     plt.legend(['theta', 'w']);
     plt.show()
+
+
+if __name__ == '__main__':
+    main()
