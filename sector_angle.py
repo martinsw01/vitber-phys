5cff95abd22aee8b958c6043bf91db8c3ff2ab36
martinsw01 <martin.s.winther@gmail.com>
2022-03-02 09:26:54 +0100

    #1 Take sigma and sigma0 as arguments to avoid circular imports

--- sector_angle.py
+++ sector_angle.py
@@ -12,11 +12,3 @@ def sector_angle_eq_derivative(beta):
 
 def calc_sector_angle(sigma, sigma0):
     return newtons_method(func=sector_angle_eq(sigma, sigma0), x0=2, fprime=sector_angle_eq_derivative)
-
-def main():
-    beta = calc_sector_angle()
-    print(f"beta={beta}")
-
-
-if __name__ == '__main__':
-    main()
