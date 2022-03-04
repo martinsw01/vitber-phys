9a898c53462215e274895cd080f36f9942c06fce
martinsw01 <martin.s.winther@gmail.com>
2022-03-04 12:00:19 +0100

    #11 Determine when the ship has capsized

---
+++ capsizing.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+from buoyancy import f
+from ode_solver import solve_ode, runge_kutta_4 as rk4
+from variables import calc_gamma, y_C0
+from viz import plot_states
+
+
+def has_capsized(w):
+    _, y_C, _, _, theta, _ = w
+    return abs(theta) > (np.pi - calc_gamma(theta, y_C)) / 2
+
+
+def solve_and_set_capsized(t0, tend, w0, h):
+    t, w = solve_ode(f, x0=t0, xend=tend, y0=w0, h=h, method=rk4)
+    theta = w.T[4]
+
+    capsize_index = np.argmax(has_capsized(w.T))
+    if capsize_index:  # i.e if not 0; argmax returns 0 if all results are False
+        w[capsize_index:] = [0, 0, 0, 0, np.sign(theta[capsize_index]) * np.pi/2, 0]
+
+    return t, w
+
+
+def capsizing_ship():
+    t0 = x_C0 = vx0 = vy0 = theta0 = 0
+    omega0 = 1.2
+    tend = 20
+
+    w0 = np.array([x_C0, y_C0, vx0, vy0, theta0, omega0])
+
+    t, w = solve_and_set_capsized(t0, tend, w0, h=0.01)
+
+    plot_states(t, *w.T)
+
+
+if __name__ == '__main__':
+    capsizing_ship()
