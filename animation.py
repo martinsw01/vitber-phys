1f1300e4f9e78733846709d4b1fcfde55ebbfc07
martinsw01 <martin.s.winther@gmail.com>
2022-03-06 14:22:10 +0100

    Use correct variables

--- animation.py
+++ animation.py
@@ -5,17 +5,11 @@ Man kan sende inn optional argumenter, disse står beskrevet i funksjonen. For e
 som inneholder lastens posisjon relativt metasenteret. Lasten vil da animeres som en rød sirkel.
 '''
 import matplotlib.pyplot as plt
-from matplotlib import animation
 import numpy as np
+from matplotlib import animation
 
+from variables import R, H
 
-
-# M = metasenteret = midt paa dekk
-# C = skipets tyngdepunkt
-
-
-R = 1  # skipets radius (m)
-h = 4 * R / (3 * np.pi)  # avstand M - C
 
 def init_anim():
     """ Initialises the animation.
@@ -23,15 +17,15 @@ def init_anim():
 
     global ax, boat, deck, last, CM, venstre_gjerde, høyre_gjerde, textbox_theory
     boat, = plt.plot([], [],
-                    color="k", linewidth=1)
+                     color="k", linewidth=1)
     deck, = plt.plot([], [], color="k", linewidth=1)
-    sea_surface, = plt.plot([-R*10, R* 10], [0, 0], color='blue', linewidth=2)  # The surface
+    sea_surface, = plt.plot([-R * 10, R * 10], [0, 0], color='blue', linewidth=2)  # The surface
     last, = plt.plot([], [], color="r", marker="o", markersize=10)
     CM, = plt.plot([], [], color="g", marker="o", markersize=10)
     venstre_gjerde, = plt.plot([], [], color="k", marker="|", markersize=25)
     høyre_gjerde, = plt.plot([], [], color="k", marker="|", markersize=25)
-    ax.set_xlim([-R*1.3, R* 1.3])
-    ax.set_ylim([-R*1.1, R* 1.1])
+    ax.set_xlim([-R * 1.3, R * 1.3])
+    ax.set_ylim([-R * 1.1, R * 1.1])
     ax.set_xlabel('$x$')
     ax.set_ylabel('$y$')
     ax.set_aspect("equal")
@@ -39,28 +33,28 @@ def init_anim():
     textbox_theory = ax.text(0.775, 0.95, '', transform=ax.transAxes, fontsize=12,
                              verticalalignment='top', bbox=props)
 
-    return ax, boat, deck, last, CM,venstre_gjerde,høyre_gjerde, textbox_theory
+    return ax, boat, deck, last, CM, venstre_gjerde, høyre_gjerde, textbox_theory
 
 
 def animate(M, theta, t, x_C, y_C, s_L, gjerde=False):
     global ax, boat, deck, last, CM, venstre_gjerde, høyre_gjerde, textbox_theory
     ax.set_xlim([-R * 1.1 + np.amin(x_C), R * 1.1 + np.amax(x_C)])
-    ax.set_ylim([-R*1.1, R* 1.1])
+    ax.set_ylim([-R * 1.1, R * 1.1])
     angle_values = np.linspace(0, np.pi, 100)
-    metasenter_x = x_C[M] - h * np.sin(theta[M])
-    metasenter_y = y_C[M] + h * np.cos(theta[M])
+    metasenter_x = x_C[M] - H * np.sin(theta[M])
+    metasenter_y = y_C[M] + H * np.cos(theta[M])
     xs = R * np.cos(angle_values + np.pi + theta[M]) + metasenter_x
     ys = R * np.sin(angle_values + np.pi + theta[M]) + metasenter_y
     boat.set_data(xs, ys)
     deck.set_data([xs[0], xs[-1]], [ys[0], ys[-1]])
-    if s_L[M] !=-42:
+    if s_L[M] != -42:
         last.set_data(metasenter_x + s_L[M] * np.cos(theta[M]),
                       metasenter_y + s_L[M] * np.sin(theta[M]))
     CM.set_data(x_C[M], y_C[M])
     if gjerde:
         venstre_gjerde.set_data([metasenter_x - R * np.cos(theta[M])], [metasenter_y - R * np.sin(theta[M])])
         høyre_gjerde.set_data([metasenter_x + R * np.cos(theta[M])], [metasenter_y + R * np.sin(theta[M])])
-    theta_string= r'$\theta = %.2f$' % (theta[M] * 180 / np.pi) + r"$\degree$"
+    theta_string = r'$\theta = %.2f$' % (theta[M] * 180 / np.pi) + r"$\degree$"
     time_string = '$t =  %.2f$' % (t[M])
     textbox_theory.set_text(
         theta_string + '\n' + time_string)
@@ -69,7 +63,7 @@ def animate(M, theta, t, x_C, y_C, s_L, gjerde=False):
     return ax, boat, deck, last, CM, venstre_gjerde, høyre_gjerde, textbox_theory
 
 
-def animate_deck_movement(t, theta, x_C, y_C, s_L=[], gjerde=False, stepsize=0.01, vis_akse_verdier=False):
+def animate_deck_movement(t, theta, x_C, y_C, s_L=None, gjerde=False, stepsize=0.01, vis_akse_verdier=False):
     """
 
     :param t: Array som inneholder tidsverdiene man har beregnet \vec{w} for systemet
@@ -90,12 +84,11 @@ def animate_deck_movement(t, theta, x_C, y_C, s_L=[], gjerde=False, stepsize=0.0
     t_anim = t[::skips]
     x_C_anim = x_C[::skips]
     y_C_anim = y_C[::skips]
-    if len(s_L) == 0:
-        s_L_anim = -42 * np.ones(len(theta_anim))
+    if s_L is None:
+        s_L_anim = - 42 * np.ones_like(theta_anim, dtype=int)
     else:
         s_L_anim = s_L[::skips]
     h_anim = animation.FuncAnimation(fig, animate, init_func=init_anim, frames=len(t_anim) - 1, interval=1,
                                      blit=not vis_akse_verdier,
                                      fargs=(theta_anim, t_anim, x_C_anim, y_C_anim, s_L_anim, gjerde))
     plt.show()
-
